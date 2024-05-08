0feeafd1a1ce99f7c814365c4a7a6420b772f26a
Juan17Informatico <juancampuzano2356@gmail.com>
2024-05-07 22:03:35 -0500

    Formato inicial para los datos en formato JSON

---
+++ src/data/AuthorsQuotes.js
@@ -0,0 +1,15 @@
+
+
+export const AuthorsQuotes = [
+    {
+        "author": "Julio Verne",
+        "birthdate": 1828,
+        "date_death": 1905,
+        "biography": "loreloremloremlorem",
+        "quotes": [
+            "lorem 1",
+            "lorem 2",
+            "lorem 3"
+        ],
+    },
+];
