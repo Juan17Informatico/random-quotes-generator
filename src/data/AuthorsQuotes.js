ebc507ff301dba4943e7b58127b1743823c19d71
Juan17Informatico <juancampuzano2356@gmail.com>
2024-05-08 22:03:46 -0500

    Agregar información a nuestro objeto JSON - Parte 1

--- src/data/AuthorsQuotes.js
+++ src/data/AuthorsQuotes.js
@@ -2,14 +2,123 @@
 
 export const AuthorsQuotes = [
     {
-        "author": "Julio Verne",
-        "birthdate": 1828,
-        "date_death": 1905,
-        "biography": "loreloremloremlorem",
-        "quotes": [
-            "lorem 1",
-            "lorem 2",
-            "lorem 3"
-        ],
+        author: "Julio Verne",
+        birthdate: 1828,
+        date_death: 1905,
+        biography: "Julio Verne, nacido el 8 de febrero de 1828 en Nantes, Francia, fue un visionario escritor conocido como el 'padre de la ciencia ficción'. A pesar de haber estudiado derecho, su pasión siempre fue la literatura. A lo largo de su vida, Verne escribió más de 60 novelas y numerosos relatos cortos, muchos de los cuales se han convertido en clásicos de la literatura mundial."
+            + "\n Su obra más famosa es 'Veinte mil leguas de viaje submarino', publicada en 1870, que relata las aventuras del Capitán Nemo a bordo del submarino Nautilus. Otras obras destacadas incluyen 'Viaje al centro de la Tierra', 'La vuelta al mundo en ochenta días' y 'De la Tierra a la Luna'. Sus historias estaban llenas de imaginación y anticipaban tecnologías que más tarde se convertirían en realidad. "
+            + "\n Verne fue un prolífico viajero y se inspiró en sus propias experiencias para muchas de sus novelas. Aunque inicialmente sus obras fueron recibidas con cierto escepticismo, con el tiempo se ganó el reconocimiento y el éxito internacional. Su influencia en la literatura y la cultura popular es innegable, y su legado perdura hasta el día de hoy, inspirando a generaciones de lectores y creadores de todo el mundo. Verne falleció el 24 de marzo de 1905 en Amiens, Francia, dejando un legado perdurable en la literatura y la imaginación humana.",
+        quotes: [
+            "Todo lo que una persona puede imaginar, otras podrán hacerlo realidad.",
+            "La ciencia, mi chico, es hecha de errores, pero son errores que uno debe cometer para aprender",
+            "La imaginación abrirá cualquier puerta, la que deseéis, pero una vez abierta, no os extrañe lo que podéis encontrar al otro lado.",
+        ]
     },
+    {
+        author: "Albert Camus",
+        birthdate: 1913,
+        date_death: 1960,
+        biography: "Albert Camus, nacido el 7 de noviembre de 1913 en Mondovi, Argelia, fue un prominente escritor, filósofo y periodista francés, cuya obra reflejó la influencia de su origen argelino y los acontecimientos de su época. Abordó temas como la absurdidad de la existencia humana, la lucha contra la injusticia y la búsqueda de sentido en un mundo sin sentido aparente. "
+            + "\nA pesar de su brillantez académica, problemas de salud interrumpieron su vida universitaria, llevándolo a dedicarse por completo a la escritura y el periodismo.Su obra más conocida, 'El extranjero'(1942), retrata la alienación y la indiferencia existencial a través del personaje de Meursault.También escribió 'La peste'(1947), una alegoría sobre el absurdo y la lucha contra el mal."
+            + "\nCamus fue un destacado ensayista y dramaturgo, conocido por obras como 'El mito de Sísifo'(1942), donde explora el absurdo de la existencia, y por obras teatrales como 'Calígula'(1944) y 'Los justos'(1950)."
+            + "\nEn términos políticos, participó en la resistencia francesa durante la Segunda Guerra Mundial y criticó el totalitarismo de ambos extremos políticos, distanciándose del comunismo debido a sus críticas a la Unión Soviética y manteniendo una postura humanista y libertaria."
+            + "\nSu vida terminó trágicamente en un accidente automovilístico el 4 de enero de 1960, pero su legado literario y filosófico perdura, siendo objeto de estudio y admiración en todo el mundo.Su obra sigue siendo relevante para entender las complejidades de la condición humana y los desafíos éticos y existenciales de la vida moderna.",
+        quotes: [ 
+            "El hombre es la única criatura que se niega a ser lo que es.",
+            "En medio del invierno, aprendí por fin que había en mí un verano invencible.",
+            "El único modo de lidiar con un mundo sin libertad es volverse tan absolutamente libre que tu mera existencia sea un acto de rebelión.",
+        ]   
+    },
+    {
+        author: "Mario Mendoza",
+        birthdate: 1964,
+        date_death: 0,
+        biography: "Mario Mendoza es un escritor colombiano nacido en Bogotá el 27 de marzo de 1964. Se graduó como filósofo de la Universidad Nacional de Colombia y obtuvo una maestría en Literatura en la Universidad de París. A lo largo de su carrera literaria, Mendoza se ha destacado por su capacidad para explorar temas sociales y políticos complejos, particularmente la violencia y la marginalidad en Colombia." 
+            + "\nUna de las obras más influyentes de Mendoza es 'Satanás'(2002), una novela que examina la violencia urbana y el caos moral en Bogotá a través de la perspectiva de un grupo de amigos.Esta novela le otorgó reconocimiento internacional y fue adaptada al cine.Otra obra destacada es 'Relato de un asesino'(1999), que aborda el tema de la violencia desde la mente perturbada de un asesino en serie."
+            + "\nMendoza ha recibido numerosos premios y reconocimientos a lo largo de su carrera, incluido el Premio Nacional de Literatura de Colombia en 2004 por su contribución al desarrollo de la literatura colombiana contemporánea.Su estilo narrativo se caracteriza por una prosa cruda y directa, así como por una habilidad para crear personajes complejos y situaciones que reflejan las realidades sociales del país."
+            + "\nAdemás de su labor como escritor, Mendoza ha incursionado en la docencia y la crítica literaria, participando en conferencias y eventos culturales tanto en Colombia como en el extranjero.Su obra continúa siendo objeto de estudio y debate en el ámbito académico y literario, consolidándolo como una figura importante en la literatura colombiana contemporánea.",
+        quotes: [
+            "La realidad es un lenguaje. Si no lo entiendes, no la entiendes.",
+            "La literatura es un modo de buscar sentido en un mundo que, a veces, parece no tenerlo",
+            "El escritor tiene la obligación de ser un testigo crítico de su tiempo, de cuestionar las verdades establecidas y de explorar las complejidades del alma humana",
+        ]
+    },
+    {
+        author: "Jorge Luis Borges",
+        birthdate: 1899,
+        date_death: 1986,
+        biography: "Jorge Luis Borges, nacido en 1899 en Buenos Aires, Argentina, fue un escritor ciego que exploró temas como la realidad, el tiempo y la metafísica en su obra. Influenciado por el ultraísmo, desarrolló un estilo único y erudito. Es conocido por obras como 'Ficciones' y 'El Aleph', donde desafía las convenciones narrativas. Su legado perdura como uno de los escritores más influyentes del siglo XX en la literatura en español. Borges falleció en 1986, dejando una huella indeleble en la literatura mundial.",
+        quotes: [
+            "No hay que temer a la realidad, sino a las ideas que tienen de ella.",
+            "El laberinto no es para que lo resolvamos, sino para que lo contemplemos.",
+            "Creo que hay un límite para la lectura, como lo hay para otras artes, aunque algunos lo nieguen.",
+        ]
+    },
+    {
+        author: "Julio Cortazar",
+        birthdate: 1914,
+        date_death: 1984,
+        biography: "Julio Cortázar, nacido el 26 de agosto de 1914 en Bruselas, Bélgica, fue un escritor argentino conocido por su innovación en la narrativa. Se trasladó a Argentina en su juventud y trabajó como profesor antes de dedicarse por completo a la escritura. Su obra más famosa, 'Rayuela', es un hito en la literatura latinoamericana, destacando por su estructura no lineal y experimental. Cortázar exploró temas como la realidad, el tiempo y la identidad en sus cuentos y novelas, convirtiéndose en una figura destacada del 'boom' latinoamericano. Falleció en París en 1984, dejando un legado literario duradero.",
+        quotes:[ 
+            "Yo escribo para mí, para los amigos que yo no tengo pero que se parecen a mí, para el hombre de la calle que no conozco pero que también se parece a mí.",
+            "Caminante, no hay camino, se hace camino al andar.",
+            "Creo que las verdaderas patrias son las infancias, esas ciudades minúsculas de los primeros años que uno recuerda y a las cuales intenta volver.",
+        ]
+    },
+    {
+        author: "Friedrich Nietzsche",
+        birthdate: 1844,    
+        date_death: 1900,
+        biography: "Friedrich Nietzsche, nacido el 15 de octubre de 1844 en Prusia, fue un influyente filósofo alemán. Su obra desafió las convenciones morales y religiosas de su tiempo, abogando por la 'voluntad de poder' y proclamando la muerte de Dios. Su crítica a la moral tradicional y su concepto del 'superhombre' han tenido un impacto duradero en la filosofía y la cultura occidental. Nietzsche sufrió problemas de salud mental durante gran parte de su vida y falleció en 1900. Su obra sigue siendo objeto de estudio y debate en la filosofía contemporánea.",
+        quotes: [
+            "Aquello que se hace por amor está siempre más allá del bien y del mal.",
+            "El hombre es una cuerda tendida entre el animal y el superhombre, una cuerda sobre un abismo.",
+            "Si miras largo tiempo a un abismo, el abismo también mira dentro de ti.",
+        ]
+    },
+    {
+        author: "Arthur Schopenhauer",
+        birthdate: 1788,
+        date_death: 1860,
+        biography: "Arthur Schopenhauer, nacido el 22 de febrero de 1788 en Danzig (actualmente Gdańsk, Polonia), fue un filósofo alemán conocido por su pesimismo filosófico y su influencia en el existencialismo y el pesimismo moderno. Estudió en las universidades de Gotinga y Berlín, donde se familiarizó con las ideas de Immanuel Kant y otros filósofos. Su obra más influyente, 'El mundo como voluntad y representación', publicada en 1818, sostiene que la voluntad es la fuerza primordial que impulsa todas las acciones humanas y naturales, y que la vida está llena de sufrimiento debido a la insaciabilidad de la voluntad. Schopenhauer murió en 1860 en Fráncfort del Meno, Alemania, dejando un legado filosófico perdurable.",
+        quotes: [
+            "La vida oscila como un péndulo hacia adelante y hacia atrás entre el dolor y el aburrimiento.",
+            "La felicidad no es algo que experimentamos, es algo que recordamos.",
+            "La lectura es para la mente lo que el ejercicio es para el cuerpo.",
+        ]
+    },
+    {
+        author: "Immanuel Kant",
+        birthdate: 1724,
+        date_death: 1804,
+        biography: "Immanuel Kant, nacido el 22 de abril de 1724 en Königsberg, Prusia (ahora Kaliningrado, Rusia), fue un influyente filósofo alemán de la Ilustración. Es conocido por su trabajo en epistemología, ética y metafísica, así como por su influyente obra 'Crítica de la razón pura', donde examina los límites del conocimiento humano. Kant desarrolló la teoría del idealismo trascendental, que sostiene que ciertos aspectos de la realidad son construidos por la mente humana. También es famoso por su formulación del imperativo categórico en ética, que establece que uno debe actuar solo según principios que puedan ser universalmente aceptados. Kant murió el 12 de febrero de 1804 en Königsberg, dejando un legado filosófico duradero.",
+        quotes:[
+            "La ciencia es organizada conocimiento. Sabiduría es organizada vida.",
+            "El hombre no es más que lo que la educación hace de él.",
+            "Dos cosas llenan el ánimo de admiración y respeto, siempre nuevos y crecientes, cuanto más frecuente y continuadamente se ocupa de ellas la reflexión: el cielo estrellado sobre mí y la ley moral dentro de mí.",
+        ]
+    },
+    {
+        author: "René Descartes",
+        birthdate: 1596,
+        date_death: 1650,
+        biography: "René Descartes, nacido el 31 de marzo de 1596 en La Haye en Touraine, Francia, fue un filósofo, matemático y científico francés. Es considerado uno de los padres de la filosofía moderna y su obra más influyente es 'Discurso del Método', donde propone su famoso principio 'Cogito, ergo sum' ('Pienso, luego existo'). Descartes hizo importantes contribuciones a la geometría analítica y a la formulación de la ley de inercia en la física. Su énfasis en la razón y el método científico tuvo un profundo impacto en el pensamiento occidental. Murió el 11 de febrero de 1650 en Estocolmo, Suecia.",
+        quotes: [
+            "Cogito, ergo sum. (Pienso, luego existo.)",
+            "La duda es el origen de la sabiduría.",
+            "Divide et impera (Divide y vencerás.)",
+        ]
+    },
+    {
+        author: "Baruch Spinoza",
+        birthdate: 1632,
+        date_death: 1677,
+        biography: "Baruch Spinoza, nacido el 24 de noviembre de 1632 en Ámsterdam, Países Bajos, fue un filósofo neerlandés de origen sefardí portugués. Es reconocido por su obra 'Ética demostrada según el orden geométrico', donde desarrolla un sistema filosófico que aborda temas como la naturaleza de Dios, el hombre y la moralidad. Spinoza defendió una visión panteísta de Dios y argumentó que todo en el universo es una manifestación de la sustancia divina. Su filosofía racionalista y su enfoque en la libertad de pensamiento lo convirtieron en un precursor de la Ilustración y del pensamiento moderno. Spinoza murió el 21 de febrero de 1677 en La Haya, Países Bajos.",
+        quotes: [
+            "La felicidad no es la recompensa de la virtud, sino que la virtud misma es la felicidad.",
+            "La mente humana no puede ser absolutamente destruida con el cuerpo, pero algo de ella queda que es eterno.",
+            "No ríes porque estés feliz; estás feliz porque ríes.",
+        ]
+    },
+        
 ];
