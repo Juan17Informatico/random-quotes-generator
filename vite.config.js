c88f30d8fd95f5645bf7f8a1161f07ae847db053
Juan17Informatico <juancampuzano2356@gmail.com>
2024-07-31 20:36:31 -0500

    Mejora del vite config

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  base: '/', // Ajusta esto si tu aplicación se despliega en un subdirectorio
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
